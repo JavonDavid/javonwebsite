c7d077e49ae9c32a3a55aa55bc2c577b44f9c7f7
J'son David <jdavid@TD48F.WPA.Dal.Ca>
2024-10-10 11:40:33 -0300

    pushing my new website im making

---
+++ NewProject/practice.js
@@ -0,0 +1,51 @@
+let p1 = 6;
+let p2 = 3;
+
+console.log(squareroot(p1,p2));
+function squareroot(p1, p2) {
+return Math.squareroot(p1,p2)
+}
+
+/*   const fruits =["apple","bananas","orange","grapes","blueberry"];
+ console.log(fruits[0]);
+ fruits.splice(0,1);
+ console.log(fruits);
+
+ fruits.push("blackberries");
+ console.log(fruits); */
+
+/* let num1 = 0;
+let max = 20;
+
+while (num1 < max) {
+    num1++;
+    console.log(num1);
+    if (num1 == max) {
+        console.log("All Done!!");
+    }
+
+} */
+
+/* let num = 50;
+if (num > 100) {
+    console.log("THATS A BIG NUMBER");
+} if (num >= 50 && num <= 100) {
+    console.log("Thats a medium number");
+} else {
+    console.log("Thats a small number");
+} */
+
+/* let firstNum = 5;
+let secNum = 10;
+let thirdNum = 15;
+let emptyNum;
+emptyNum = firstNum + secNum;
+let result = emptyNum * thirdNum;
+
+console.log(result);
+ */
+
+//let firstName = "Javon";
+//let age = 24;
+
+//console.log(`Your name is ${firstName} and your age is ${age}`); 
